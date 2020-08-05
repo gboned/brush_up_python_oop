70d64d1327f228a618c68b294f8c7d0875e0780b
Garbiñe <gboned@cifpfbmoll.eu>
2020-08-05 22:47:07 +0200

    feat(Teacher): Añadir métodos de clase

--- teacher/Teacher.py
+++ teacher/Teacher.py
@@ -3,5 +3,14 @@ from person.Person import Person
 class Teacher(Person):
 
     def __init__(self, name, age, patience):
-        super().__init__(self, name, age)
+        super().__init__(name, age)
         self.patience = patience
+
+    
+
+    def getPatience(self):
+        return self.patience
+
+
+    def setPatience(self, newPatience):
+        self.patience = newPatience
